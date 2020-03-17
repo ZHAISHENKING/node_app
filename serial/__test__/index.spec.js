79c6c3d0fb60954a1935026ed92a2d1a2bc80c9c
ZHAISHENKING <18700790825@163.com>
2020-03-17 14:58:20 +0800

    异步编程

---
+++ serial/__test__/index.spec.js
@@ -0,0 +1,30 @@
+const {callback, promise, generator, asyncAwait, event} = require('../index')
+
+describe('异步', () => {
+
+  // test('callback', done => {
+  //   callback()
+  //   // 延时4s结束
+  //   setTimeout(done, 1000)
+  // })
+  //
+  // test('promise', done => {
+  //   promise()
+  //
+  //   // 延时4s结束
+  //   setTimeout(done, 1000)
+  // })
+  //
+  // test('generator', done => {
+  //   const {generator} = require('../index')
+  //   generator()
+  //   setTimeout(done, 1000)
+  // })
+
+
+  test('asyncAwait', async (done) => {
+    await asyncAwait()
+    setTimeout(done, 1000)
+  })
+
+})
